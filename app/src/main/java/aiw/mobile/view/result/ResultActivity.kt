287e5d769d8c7b4690caa0f2dd524883a7a86830
Aldy Imam Wijaya <aldyimam03@gmail.com>
2024-06-07 09:45:32 +0700

    upgrade camera feature

--- app/src/main/java/aiw/mobile/view/result/ResultActivity.kt
+++ app/src/main/java/aiw/mobile/view/result/ResultActivity.kt
@@ -1,21 +1,33 @@
 package aiw.mobile.view.result
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import aiw.mobile.testonboardingpage.R
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import aiw.mobile.testonboardingpage.databinding.ActivityResultBinding
+import aiw.mobile.view.dashboard.BottomActivity
+import aiw.mobile.view.dashboard.ui.dashboard.DashboardFragment
+import android.content.Intent
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageUri = intent.getStringExtra("IMAGE_URI")
+        if (imageUri != null) {
+            binding.ivResultCamera.setImageURI(Uri.parse(imageUri))
+        } else {
+            // Handle error if imageUri is null
+        }
+
+        binding.ivBack.setOnClickListener {
+            val intent = Intent(this, BottomActivity::class.java)
+            startActivity(intent)
         }
     }
 }
